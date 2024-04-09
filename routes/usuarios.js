c1cc292427c64664d73f184efb0c16623d545d83
Bianca Oliveira <bianca.oliv.bn@gmail.com>
2024-04-09 16:10:22 -0300

    add: rota de autenticação de usuário email e senha

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,8 +6,8 @@ const bcrypt = require("bcrypt");
 router.post("/cadastro", (req, res, next) => {
   mysql.getConnection((err, conn) => {
     if (err) {
-      return res.status(500).send({ error: error });
-    }
+      return res.status(500).send({ error: error })
+    };
     conn.query(
       "SELECT * FROM usuarios WHERE email = ?",
       [req.body.email],
@@ -45,6 +45,29 @@ router.post("/cadastro", (req, res, next) => {
       }
     );
   });
+});
+
+router.post('/login', (req, res, next) => {
+      mysql.getConnection((error, conn) => {
+        if(error) { return res.status(500).send({ error: error})}
+          const query = `SELECT * FROM usuarios WHERE email= ?`
+          conn.query(query, [req.body.email], (error, results, fields) => {
+            conn.release();
+          if (error) { return res.status(500).send({ error: error })}
+          if (results.length < 1) {
+            return res.status(401).send({ mensagem: 'Falha na autenticação' })
+          }
+          bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
+            if (err) {
+              return res.status(401).send({ mensagem: 'Falha na autenticação'})
+            }
+            if (result) {
+              return res.status(200).send({ mensagem: 'Autenticado com sucesso'})
+            }
+            return res.status(401).send({ mensagem: 'Falha na autenticação'})
+          })
+        })
+      });
 });
 
 module.exports = router;
