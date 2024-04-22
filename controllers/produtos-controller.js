8284c4c1bf4101df0d4cfcbe584788a0a3b338d0
Bianca Oliveira <bianca.oliv.bn@gmail.com>
2024-04-21 23:22:20 -0300

    add: tabela de imagens

--- controllers/produtos-controller.js
+++ controllers/produtos-controller.js
@@ -2,41 +2,39 @@ const mysql = require('../mysql');
 
 exports.getProdutos = async (req, res, next) => {
     try {
-        const result = await mysql.execute('SELECT * FROM produtos')
-            const response = {
-                quantidade: result.length,
-                produtos: result.map(prod => {
-                    return {
-                        id_produto: prod.id_produto,
-                        nome: prod.nome,
-                        preco: prod.preco,
-                        imagem_produto: prod.imagem_produto,
-                        request: {
-                            tipo: 'GET',
-                            descricao:
-                                'Retorna os detalhes de um produto específico',
-                            url:
-                                'http://localhost:3000/produtos/' +
-                                prod.id_produto,
-                        },
-                    };
-                }),
-            };
-            return res.status(200).send(response);
+        const result = await mysql.execute('SELECT * FROM produtos');
+        const response = {
+            quantidade: result.length,
+            produtos: result.map(prod => {
+                return {
+                    id_produto: prod.id_produto,
+                    nome: prod.nome,
+                    preco: prod.preco,
+                    imagem_produto: prod.imagem_produto,
+                    request: {
+                        tipo: 'GET',
+                        descricao:
+                            'Retorna os detalhes de um produto específico',
+                        url:
+                            process.env.URL_API + 'produtos/' + prod.id_produto,
+                    },
+                };
+            }),
+        };
+        return res.status(200).send(response);
     } catch (error) {
         return res.status(500).send({ error: error });
     }
+};
 
-}
-
-
-exports.postProduto = (req, res, next) => {
+exports.postProduto = async (req, res, next) => {
     try {
-        const query = 'INSERT INTO produtos (nome, preco, imagem_produto) VALUES (?,?,?)'
-        const result = mysql.execute(query, [
+        const query =
+            'INSERT INTO produtos (nome, preco, imagem_produto) VALUES (?,?,?)';
+        const result = await mysql.execute(query, [
             req.body.nome,
             req.body.preco,
-            req.file.path
+            req.file.path,
         ]);
         const response = {
             mensagem: 'Produto inserido com sucesso',
@@ -48,7 +46,7 @@ exports.postProduto = (req, res, next) => {
                 request: {
                     tipo: 'GET',
                     descricao: 'Retorna todos os produtos',
-                    url: 'http://localhost:3000/produtos',
+                    url: process.env.URL_API + 'produtos',
                 },
             },
         };
@@ -58,10 +56,10 @@ exports.postProduto = (req, res, next) => {
     }
 };
 
-exports.getUmProduto = async(req, res, next) => {
+exports.getUmProduto = async (req, res, next) => {
     try {
-        const query = 'SELECT * FROM produtos WHERE id_produto = ?;'
-        const result = await mysql.execute(query, [ req.params.id_produto]);
+        const query = 'SELECT * FROM produtos WHERE id_produto = ?;';
+        const result = await mysql.execute(query, [req.params.id_produto]);
         if (result.length == 0) {
             return res.status(404).send({
                 mensagem: 'Não foi encontrado produto com este ID',
@@ -76,7 +74,7 @@ exports.getUmProduto = async(req, res, next) => {
                 request: {
                     tipo: 'GET',
                     descricao: 'Retorna todos os produtos',
-                    url: process.env.URL_API + 'produtos',
+                    url: process.env.URL_API + 'produtos/',
                 },
             },
         };
@@ -94,7 +92,7 @@ exports.updateProduto = async (req, res, next) => {
         await mysql.execute(query, [
             req.body.nome,
             req.body.preco,
-            req.body.id_produto
+            req.body.id_produto,
         ]);
         const response = {
             mensagem: 'Produto atualizado com sucesso',
@@ -104,17 +102,15 @@ exports.updateProduto = async (req, res, next) => {
                 preco: req.body.preco,
                 request: {
                     tipo: 'GET',
-                    descricao:
-                        'Retorna os detalhes de um produto específico',
+                    descricao: 'Retorna os detalhes de um produto específico',
                     url:
-                        'http://localhost:3000/produtos/' +
-                        req.body.id_produto,
+                        process.env.URL_API + 'produtos/' + req.body.id_produto,
                 },
             },
         };
         return res.status(202).send(response);
     } catch (error) {
-        return res.status(500).send({ error: error }); 
+        return res.status(500).send({ error: error });
     }
 };
 
